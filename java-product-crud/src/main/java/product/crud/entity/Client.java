142e405bbbeffe19db929bce9cd8ffc10c452ea2
Pedro <pedrohlucena413@gmail.com>
2023-03-17 18:45:30 -0300

    add inheritance policy for Client entity

--- java-product-crud/src/main/java/product/crud/entity/Client.java
+++ java-product-crud/src/main/java/product/crud/entity/Client.java
@@ -3,16 +3,21 @@ package product.crud.entity;
 import java.util.List;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="T_CLIENT")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="type")
 public class Client {
 
 	@Id
